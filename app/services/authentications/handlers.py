44ed5895a3f29cadab3b5382c43eb4935a6a09ca
MUBARAK-SSR <mubarak_i@sygalin.com>
2025-09-23 13:07:34 +0100

    remove bug

--- app/services/authentications/handlers.py
+++ app/services/authentications/handlers.py
@@ -50,6 +50,3 @@ async def authenticate_user(data: AuthenticationBase, db: AsyncSession):
     print(f"identifiant de l'utilisateur: {user.id}")
 
     return {"message": "Access granted", "token": token}
-
-def buggy_function():
-    x = 1 / 0
