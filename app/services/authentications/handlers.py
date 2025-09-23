38d049e58f02883772383a2a827c90711e2c5bd6
MUBARAK-SSR <mubarak_i@sygalin.com>
2025-09-23 12:12:33 +0100

    add Semgrep quality-gate workflow

--- app/services/authentications/handlers.py
+++ app/services/authentications/handlers.py
@@ -50,8 +50,3 @@ async def authenticate_user(data: AuthenticationBase, db: AsyncSession):
     print(f"identifiant de l'utilisateur: {user.id}")
 
     return {"message": "Access granted", "token": token}
-
-def example():
-    x = 42  # unused variable
-    PASSWORD = "123456"  # security hotspot
-    print(PASSWORD)
