35dd78147dcfeee653494154590c6b6f19875562
MUBARAK-SSR <mubarak_i@sygalin.com>
2025-09-23 12:26:55 +0100

    add security hotspot

--- app/services/authentications/handlers.py
+++ app/services/authentications/handlers.py
@@ -50,3 +50,7 @@ async def authenticate_user(data: AuthenticationBase, db: AsyncSession):
     print(f"identifiant de l'utilisateur: {user.id}")
 
     return {"message": "Access granted", "token": token}
+
+def example():
+    PASSWORD = "123456"  # security hotspot
+    print(PASSWORD)
