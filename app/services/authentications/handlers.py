bc18d60f218d607d7518af82749231eb2a873b42
MUBARAK-SSR <mubarak_i@sygalin.com>
2025-09-23 12:23:26 +0100

    remove code smell

--- app/services/authentications/handlers.py
+++ app/services/authentications/handlers.py
@@ -50,6 +50,3 @@ async def authenticate_user(data: AuthenticationBase, db: AsyncSession):
     print(f"identifiant de l'utilisateur: {user.id}")
 
     return {"message": "Access granted", "token": token}
-
-def example():
-    x = 42
