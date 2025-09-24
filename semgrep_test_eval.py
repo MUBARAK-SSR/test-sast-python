cdd2aa10436471161b6a1dc681e6aecab88fff0c
MUBARAK-SSR <mubarak_i@sygalin.com>
2025-09-25 00:17:57 +0100

    add injection

--- semgrep_test_eval.py
+++ semgrep_test_eval.py
@@ -1,3 +1,15 @@
+import os
+from flask import request, Flask
+
+app = Flask(__name__)
+
+@app.route("/ping")
+def ping():
+    host = request.args.get("host")
+    # Vulnérable : injection possible si l'utilisateur passe ; rm -rf /
+    return os.popen(f"ping -c 1 {host}").read()
+
+
 def dangerous_eval(user_input):
-    #Intentional vuln: using eval on user-controlled input
+    # Intentional vuln: using eval on user-controlled input
     return eval(user_input)
